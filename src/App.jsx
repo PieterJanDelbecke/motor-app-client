20454bb4619b804927c18aadea3ed91695f59b37
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-07-31 15:08:48 +1000

    feat: add Home and SignUp Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Home from "./components/home"
+import SignUp from "./components/signUp"
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/signUp.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const SignUp = () => {
+    return(
+        <>
+            <Link to="/">Home</Link>
+            <h1>Sign Up Page</h1>
+        </>
+    )
+}
+
+export default SignUp
